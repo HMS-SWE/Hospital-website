82406fa71383ded66427490ce26e4f69a4c60537
Mohamed Hossam <ma7amedhossam@gmail.com>
2026-04-26 15:35:15 +0300

    Re-designed the registration form

--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -2,39 +2,72 @@ import Input from './input'
 import Button from './Button/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import CheckBox from './CheckBox';
 
 function Register(){
     return(
         <>
-        <div className='Container'>
+        <div className='Square'>
+
             <div className='LHS'>
-                <h1 className='RegisterMessage'>Welcome to our Hospital System</h1>
+                <div className='Hospital-Headline'>
+                    <h1>Hospital Management System</h1>
+                    <span>Streamline your healthcare operations with our comprehensive patient management platform</span>
+                </div>
             </div>
-            <div className='RHS'>
-                <h2>Please fill-in your details</h2>
-                <div className='Data'>
+
+            <div className="RHS">
+            <div className="Headline">
+                <h1>Patient Registration</h1>
+                <span>Create your account</span>
+            </div>
+            <div className="Data">
+                <div className="Name">
                     <Input label="First name:" type="text" />
-                    <div className="Field">
+                    <Input label="Middle name:" type="text" />
+                    <Input label="Last name:" type="text" />
+                </div>
+                <div className="ID-DOB">
+                    <Input label="National ID:" type="text" />
+                    <Input label="Date of Birth:" type="date" />
+                </div>
+                <div className="genderField">
                         <label>Gender:</label>
                         <br></br>
                         <select className='gender'>
                         <option>Male</option>
                         <option>Female</option>
                         </select>
-                    </div>
-                    <Input label="Middle name:" type="text" />
-                    <Input label="Phone number:" type="text" />
-                    <Input label="Last name:" type="text" />
-                    <Input label="E-mail:" type="email" />
-                    <Input label="Date of Birth:" type="date"/>
-                    <Input label="Password:" type="password" />
                 </div>
-                <Button />
-                <span>Or register using: 
-                    <a href='#'><FontAwesomeIcon icon={faGoogle} /></a>
-                </span>
+                <div className='mail-phone'>
+                    <Input label="Email" type="text" />
+                    <Input label="Phone Number" type="text" />
+                </div>
+                <div className='Em-Contact'>
+                    <Input label="Emergency Contact" type="text" />
+                </div>
+                <div className='Pass'>
+                    <Input label="Password" type="password" />
+                </div>
+                <div className='checklist'>
+                    <span>Chronic disease (if any):</span>
+                    <div className='checks'>
+                        <div><CheckBox name="diabetes" title="Diabetes" /></div>
+                        <div ><CheckBox name="heart" title="Heart disease" /></div>
+                        <div><CheckBox name="pressure" title="Blood pressure disease" /></div>
+                        <div><CheckBox name="immune" title="Immune system disease" /></div>                        
+                    </div>
+                </div>
+                <button className='submitButton'>Register</button>
+                <div className='divider'>Or continue with:</div>
+                <button className='googleButton'><img className='googleIcon' src='./public/icons8-google.svg' alt="User Icon" />Register with google</button>
+                <h5>Already have an account?<a href='#'>Login</a></h5>
             </div>
-            </div>  
+            
+            </div>
+        </div>
+        
+        
         </>
     );
 }
