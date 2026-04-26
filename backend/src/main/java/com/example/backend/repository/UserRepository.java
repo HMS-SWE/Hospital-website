3715f3f9ea2caeeef4dad9b567bda8934bb47b11
NadaMohamed2422 <nadamuhammaad242@gmail.com>
2026-04-27 01:52:42 +0300

    adding findByEmail in user repository

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -4,9 +4,15 @@ import com.example.backend.entity.User;
 import com.example.backend.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
     boolean existsByRole(Role role);
+
+    // If the email exists it contains a User
+    // If the email does not existit contains nothing
+    Optional<User> findByEmail(String email);
 }
