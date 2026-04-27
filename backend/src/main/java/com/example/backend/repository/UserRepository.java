6fa025023d0f65a2d20c757e26650df945aca83a
naduto <nadagamal5078@gmail.com>
2026-04-27 04:58:31 +0300

    a33333333

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
     boolean existsByRole(Role role);
 }
