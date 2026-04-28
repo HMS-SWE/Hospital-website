bd31590d443033e4b5263f40914dc0a433b41f1b
Aboelwafa1911 <ahmdehap191@gmail.com>
2026-04-29 02:28:25 +0300

    ` savee

--- backend/src/main/java/com/example/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/example/backend/repository/AppointmentRepository.java
@@ -1,5 +1,11 @@
 package com.example.backend.repository;
 
-public class AppointmentRepository {
+import com.example.backend.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+      
+       List<Appointment> findByPatientId(Long patientId);
+         List<Appointment> findByDoctorId(Long doctorId);
 }
