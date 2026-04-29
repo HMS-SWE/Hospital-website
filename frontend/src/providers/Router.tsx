0de66150c7408f6be8fc0b4e045d192821452c45
Mohamed Hossam <ma7amedhossam@gmail.com>
2026-04-29 23:38:03 +0300

    OAuth2 integrated successfully. Remaining redirection to patient dashboard

--- frontend/src/providers/Router.tsx
+++ frontend/src/providers/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../Pages/Register";
 import MainLayout from "../Layouts/MainLayout";
+import OAuthCallback from "../Pages/OAuthCallback";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,9 @@ const router = createBrowserRouter([
                 {
                     path: '/',
                     element: <Register />,
+                },{
+                    path: "/oauth-callback",   // add this
+                    element: <OAuthCallback />,
                 },
     ]}
 
