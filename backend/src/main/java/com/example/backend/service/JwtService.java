260ddbefaaa2e8e2d6a0f23e0570dd09637911c6
Alaa Muhammad Abdul Azim Ali Amara <es-alaa.mohamed2027@alexu.edu.eg>
2026-04-29 06:03:29 +0300

    added the resources packeges in test and added h2 dependency in pom.xml

--- backend/src/main/java/com/example/backend/service/JwtService.java
+++ backend/src/main/java/com/example/backend/service/JwtService.java
@@ -75,7 +75,6 @@ public class JwtService {
                 .getPayload();
     }
 
-<<<<<<< HEAD
     public LocalDateTime extractIssuedAt(String token) {
         Date issuedAt = extractAllClaims(token).getIssuedAt();
         return issuedAt.toInstant()
@@ -83,8 +82,6 @@ public class JwtService {
                 .toLocalDateTime();
     }
 
-=======
->>>>>>> 8ca1598 (implement Swagger/OpenAPI documentation with JWT support)
 
     private SecretKey buildSigningKey(String secret) {
         byte[] keyBytes;
