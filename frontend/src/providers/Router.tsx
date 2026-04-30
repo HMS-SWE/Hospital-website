8cd2939e9451ef3ef7becf1147f7f307fba142f1
Carl Sherif Kheir Eskander <145164885+carlsherif@users.noreply.github.com>
2026-04-30 23:20:41 +0300

    adsfv

--- frontend/src/providers/Router.tsx
+++ frontend/src/providers/Router.tsx
@@ -1,11 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import Register from "../Pages/Register";
 import MainLayout from "../Layouts/MainLayout";
 import OAuthCallback from "../Pages/OAuthCallback";
-import DashboardLayout from "../Layouts/DashboardLayout";
-import Appointments from "../Pages/Dashboard/Appointments/Appointments";
-import Dashboard from "../Pages/Dashboard/Dashboard/Dashboard"
-import BookAppointment from "../Pages/Dashboard/BookAppointment/BookAppointment";
+import { LoginForm } from "../Components/LoginForm";
+import Register from "../Pages/Register";
 
 const router = createBrowserRouter([
     {
@@ -14,20 +11,15 @@ const router = createBrowserRouter([
             children: [
                 {
                     path: '/',
-                    element: <Register />,
+                    element: <LoginForm />,
                 },{
                     path: "/oauth-callback",   // add this
                     element: <OAuthCallback />,
                 },
                 {
-                    path: "dashboard",
-                    element: <DashboardLayout />,
-                    children: [
-                        {index: true, element: <Dashboard />},
-                        {path: "appointments", element: <Appointments />},
-                        {path: "book-appointment", element: <BookAppointment /> }
-                    ]
-                }
+                    path: '/register',
+                    element: <Register/>,
+                },
     ]}
 
 ]);
