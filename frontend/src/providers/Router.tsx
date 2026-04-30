d50d237365e946f02c934eebcc1a045f73252969
Mohamed Hossam <ma7amedhossam@gmail.com>
2026-04-30 17:27:15 +0300

    Appointments Page

--- frontend/src/providers/Router.tsx
+++ frontend/src/providers/Router.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../Pages/Register";
 import MainLayout from "../Layouts/MainLayout";
+import OAuthCallback from "../Pages/OAuthCallback";
+import DashboardLayout from "../Layouts/DashboardLayout";
+import Appointments from "../Pages/Dashboard/Appointments/Appointments";
+import Dashboard from "../Pages/Dashboard/Dashboard/Dashboard"
 
 const router = createBrowserRouter([
     {
@@ -10,7 +14,18 @@ const router = createBrowserRouter([
                 {
                     path: '/',
                     element: <Register />,
+                },{
+                    path: "/oauth-callback",   // add this
+                    element: <OAuthCallback />,
                 },
+                {
+                    path: "dashboard",
+                    element: <DashboardLayout />,
+                    children: [
+                        {index: true, element: <Dashboard />},
+                        {path: "appointments", element: <Appointments />}
+                    ]
+                }
     ]}
 
 ]);
