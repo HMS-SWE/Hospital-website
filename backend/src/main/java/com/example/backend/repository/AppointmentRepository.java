a4da04dcdef2e7e9d6159913e9372aba0a0d5983
NadaMohamed2422 <nadamuhammaad242@gmail.com>
2026-05-09 19:48:24 +0300

    added a query to the appointment repo to return all doctor's non cancelled appointments , update the service to call it and update its test unit , in addition to docotor appointment view dto

--- backend/src/main/java/com/example/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/example/backend/repository/AppointmentRepository.java
@@ -1,11 +1,45 @@
 package com.example.backend.repository;
 
 import com.example.backend.entity.Appointment;
+import com.example.backend.enums.AppointmentStatus;
+import com.example.backend.dto.appointment.DoctorAppointmentView;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-      
-       List<Appointment> findByPatientId(Long patientId);
-         List<Appointment> findByDoctorId(Long doctorId);
+
+    List<Appointment> findByPatientId(Long patientId);
+
+    List<Appointment> findByDoctorId(Long doctorId);
+
+    //added cancelled as an argument to the query to make reusable 
+    //incase we wanted to exclude any other types of appointments from the results
+    @Query("""
+        SELECT new com.example.backend.dto.appointment.DoctorAppointmentView(
+            a.id,
+            p.fullName,
+            ts.startTime,
+            ts.endTime,
+            a.status,
+            a.notes,
+            a.examinationPrice
+        )
+        FROM Appointment a
+        JOIN a.timeSlot ts
+        JOIN a.patient p
+        WHERE a.doctor.id = :doctorId
+          AND ts.date     = :today
+          AND a.status   != :cancelled
+        ORDER BY ts.startTime ASC
+    """)
+    List<DoctorAppointmentView> findTodaysAppointmentsForDoctor(
+            @Param("doctorId")  Long doctorId,
+            @Param("today")     LocalDate today,
+            @Param("cancelled") AppointmentStatus cancelled
+    );
 }
